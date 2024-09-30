fb1473803a5cd57fb3163a910d2c66a2dd6ad577
AbdelwadoudMakh55 <abdelwadoud315@gmail.com>
2024-09-30 20:43:24 +0100

    Serve static files

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, send_from_directory
 from word_cloud import generate_text, generate_word_cloud
 from flask_cors import CORS
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static')
 CORS(app)
 
 @app.route('/word_cloud', methods=['POST'])
@@ -12,6 +12,13 @@ def word_cloud():
     comment_words = generate_text(text)
     return jsonify({"cloud": generate_word_cloud(comment_words, language)})
     
-    
+@app.route('/')
+def serve_frontend():
+    return send_from_directory(app.static_folder, 'index.html')
+
+@app.route('/<path:path>')
+def serve_static(path):
+    return send_from_directory(app.static_folder, path)
+
 if __name__ == '__main__':
     app.run(port=5000)
